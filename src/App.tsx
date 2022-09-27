338655c76ad79938a3257328b84435ac21ec4788
sunlight621917 <sunlight621917@gmail.com>
2022-09-27 11:07:45 +0900

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center font-bold  h-1200 bg-black md:bg-primary-400">
+      Hello World!!!
+    </div>
+  );
+}
+export default App;
